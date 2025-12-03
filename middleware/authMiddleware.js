ebeb9fadeab74fad7031dac66e6dfdb845a99d82
ocjdagreat1 <oparachibuikejustine@gmail.com>
2025-12-03 12:47:25 -0800

    new

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -6,7 +6,7 @@ let token;
 
 if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
   try {
-    token = req.headers.authorization.split("")[1];
+    token = req.headers.authorization.split(" ")[1];
     const decoded = jwt.verify(token,process.env.SECRET_KEY);
     req.user = await User.findById(decoded.id).select("-password");
     next();
